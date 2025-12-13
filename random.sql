dd12be72b6d49e1031b4328f9c654e102ebbc791
CoolCoder <aima13islam@gmail.com>
2025-12-13 14:33:20 +1100

    Update and rename random.py to random.sql

---
+++ random.sql
@@ -0,0 +1,22 @@
+--Top 15 Popular Pets Worldwide. My fav's birds - write in the chat what yours is!
+create table pets(
+RANK integer,
+ANIMAL text,
+'' text
+);
+insert into pets values (1,'Dogs','🐕');
+insert into pets values (2,'Cats','🐈');
+insert into pets values (3,'Fish','🐟');
+insert into pets values (4,'Birds','🐦');
+insert into pets values (5,'Hamsters','🐹');
+insert into pets values (6,'Guinea Pigs','❔');
+insert into pets values (7,'Rabbits','🐇');
+insert into pets values (8,'Turtles','🐢');
+insert into pets values (9,'Ferrets','❓');
+insert into pets values (10,'Lizards','🦎');
+insert into pets values (11,'Snakes','🐍');
+insert into pets values (12,'Horses','🐴');
+insert into pets values (13,'Mice','🐁');
+insert into pets values (14,'Frogs','🐸');
+insert into pets values (15,'Spiders','🕷️');
+select * from pets
